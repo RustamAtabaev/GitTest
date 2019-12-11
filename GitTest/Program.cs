d6ad49d2b5ac057f4b0cc44e31f1d7c894f89ebc
Rustam <rustam.atabaev.work@gmail.com>
2019-12-11 11:53:41 +0300

    merge dev first

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -6,27 +6,8 @@ namespace GitTest
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Develop logic");
+      Console.WriteLine("first");
 
-      Console.WriteLine("New Develop Logic");
-
-      Console.WriteLine("One more from dev");
-
-      Console.WriteLine("dev");
-
-      Console.WriteLine("front");
-
-      Console.WriteLine("test");
-
-      Console.ReadLine();
-
-      Console.WriteLine("new line");
-
-      Console.WriteLine("new line");
-
-      Console.WriteLine("new line");
-
-      Console.WriteLine("dev");
     }
   }
 }
