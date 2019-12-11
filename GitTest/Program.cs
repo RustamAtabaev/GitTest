a75b0a8bd918d27023333d64fa974d3f17c96573
Rustam <rustam.atabaev.work@gmail.com>
2019-12-11 11:54:12 +0300

    merge front second

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -8,26 +8,6 @@ namespace GitTest
     {
       Console.WriteLine("Develop logic");
 
-      Console.WriteLine("New Develop Logic");
-
-      Console.WriteLine("One more from dev");
-
-      Console.WriteLine("dev");
-
-      Console.WriteLine("front");
-
-      Console.WriteLine("test");
-
-      Console.ReadLine();
-
-      Console.WriteLine("new line");
-
-      Console.WriteLine("new line");
-
-      Console.WriteLine("new line");
-
-      Console.WriteLine("dev");
-
     }
   }
 }
