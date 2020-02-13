e869adb01e602d69100b1f9710c4011961e3c116
Cyldrian <rustam.atabaev.work@gmail.com>
2020-02-13 21:24:52 +0300

    new m1

---
+++ GitTest/master.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTest
+{
+    class master
+    {
+        public void Mast()
+        {
+            Console.WriteLine("m1");
+        }
+    }
+}
